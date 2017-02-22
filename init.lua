4d8c2610bc3bb6529643f62aea6c8e043be9ff13
Gael-de-Sailly <gael.chretien@akeonet.com>
2017-02-22 17:54:43 +0100

    Reduced default radius (24) and max radius (32) And round to the nearest chunk instead of rounding up: the radius you put has never been the exact radius taken, it was rounded to a chunk border up to 15 nodes farther. Now it's rounded to a chunk border from 8 nodes closer to 7 nodes farther.

--- init.lua
+++ init.lua
@@ -7,15 +7,15 @@ end
 
 local previous = os.time()
 
-local default_size = tonumber(minetest.setting_get("mapfix_default_size")) or 40
-local max_size = tonumber(minetest.setting_get("mapfix_max_size")) or 50
+local default_size = tonumber(minetest.setting_get("mapfix_default_size")) or 24
+local max_size = tonumber(minetest.setting_get("mapfix_max_size")) or 32
 local delay = tonumber(minetest.setting_get("mapfix_delay")) or 15
 
 minetest.register_chatcommand("mapfix", {
 	params = "<size>",
 	description = "Recalculate the flowing liquids and the light of a chunk",
 	func = function(name, param)
-		local pos = minetest.get_player_by_name(name):getpos()
+		local pos = vector.round(minetest.get_player_by_name(name):getpos())
 		local size = tonumber(param) or default_size
 		local privs = minetest.check_player_privs(name, {server=true})
 		local time = os.time()
@@ -29,8 +29,10 @@ minetest.register_chatcommand("mapfix", {
 			previous = time
 		end
 
-		local minp = vector.round(vector.subtract(pos, size - 0.5))
-		local maxp = vector.round(vector.add(pos, size + 0.5))
+		size = math.max(math.floor(size - 8), 0) -- When passed to get_voxel_manip, positions are rounded up, to a multiple of 16 nodes in each direction. By subtracting 8 it's rounded to the nearest chunk border. max is used to avoid negative radius.
+
+		local minp = vector.subtract(pos, size)
+		local maxp = vector.add(pos, size)
 
 		minetest.log("action", name .. " uses mapfix at " .. minetest.pos_to_string(vector.round(pos)) .. " with radius " .. size)
 		mapfix(minp, maxp)
