d4e9de50a98544923be71f5be49139ed25e52cfc
Gael-de-Sailly <gael.chretien@akeonet.com>
2017-02-22 18:24:35 +0100

    Limit radius to 120 even for players with server privilege.

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ local function mapfix(minp, maxp)
 	vm:update_liquids()
 	vm:write_to_map()
 	vm:update_map()
+	local emin, emax = vm:get_emerged_area()
+	print(minetest.pos_to_string(emin), minetest.pos_to_string(emax))
 end
 
 local previous = os.time()
@@ -17,6 +19,10 @@ minetest.register_chatcommand("mapfix", {
 	func = function(name, param)
 		local pos = vector.round(minetest.get_player_by_name(name):getpos())
 		local size = tonumber(param) or default_size
+
+		if size >= 121 then
+			return false, "Radius is too big"
+		end
 		local privs = minetest.check_player_privs(name, {server=true})
 		local time = os.time()
 
@@ -29,12 +35,13 @@ minetest.register_chatcommand("mapfix", {
 			previous = time
 		end
 
+		minetest.log("action", name .. " uses mapfix at " .. minetest.pos_to_string(vector.round(pos)) .. " with radius " .. size)
+
 		size = math.max(math.floor(size - 8), 0) -- When passed to get_voxel_manip, positions are rounded up, to a multiple of 16 nodes in each direction. By subtracting 8 it's rounded to the nearest chunk border. max is used to avoid negative radius.
 
 		local minp = vector.subtract(pos, size)
 		local maxp = vector.add(pos, size)
 
-		minetest.log("action", name .. " uses mapfix at " .. minetest.pos_to_string(vector.round(pos)) .. " with radius " .. size)
 		mapfix(minp, maxp)
 		return true, "Done."
 	end,
